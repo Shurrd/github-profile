d2054ed93202a8b8f6add4d2e4330b7639d3a8d0
shurrd <engraaadeyemi@gmail.com>
2023-07-21 16:01:45 +0100

    seo integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,24 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { FaGithub } from "react-icons/fa";
-import SearchBar from "../components/SearchBar";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { FaGithub } from 'react-icons/fa';
+import SearchBar from '../components/SearchBar';
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
         <title>OctoProfile</title>
+        <meta
+          name='google-site-verification'
+          content='Zfl93BKYgCaKNo5g2z3Nmfzgqv57I-Ceri7GZvN17sY'
+        />
       </Head>
-      <div className="bg-[#24292e] w-full h-[100vh] flex justify-center items-center flex-col gap-8 pb-48">
-        <div className="flex flex-col items-center justfify-center gap-5">
-          <p className="text-blue-600 text-6xl md:text-7xl">
+      <div className='bg-[#24292e] w-full h-[100vh] flex justify-center items-center flex-col gap-8 pb-48'>
+        <div className='flex flex-col items-center justfify-center gap-5'>
+          <p className='text-blue-600 text-6xl md:text-7xl'>
             <FaGithub />
           </p>
-          <p className="text-center text-gray-200 text-3xl md:text-4xl font-semibold tracking-wide">
+          <p className='text-center text-gray-200 text-3xl md:text-4xl font-semibold tracking-wide'>
             Find Your OctoProfile
           </p>
         </div>
